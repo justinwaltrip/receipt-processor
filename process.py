abf12a4869b8724dd631d05d6830d9bd47c5583b
Justin Waltrip <justin.waltrip@protonmail.com>
2023-04-29 09:18:25 -0400

    working

--- process.py
+++ process.py
@@ -1,5 +1,7 @@
 import os
-import datetime
+from dateutil.parser import parse
+import pytesseract
+from typing import Optional
 import pdf2image
 from transformers import pipeline
 from dotenv import load_dotenv
@@ -14,9 +16,64 @@ nlp = pipeline(
     model="impira/layoutlm-document-qa",
 )
 
-# zero-shot image classification pipeline
-model_name = "openai/clip-vit-large-patch14-336"
-classifier = pipeline("zero-shot-image-classification", model=model_name)
+# zero-shot text classification pipeline
+classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
+
+
+def normalize_box(box, width, height):
+    return [
+        int(1000 * (box[0] / width)),
+        int(1000 * (box[1] / height)),
+        int(1000 * (box[2] / width)),
+        int(1000 * (box[3] / height)),
+    ]
+
+
+def apply_tesseract(
+    image: "Image.Image",
+    lang: Optional[str] = "eng",
+    tesseract_config: Optional[str] = "",
+):
+    """Applies Tesseract OCR on a document image, and returns recognized words + normalized bounding boxes."""
+    # apply OCR
+    data = pytesseract.image_to_data(
+        image, lang=lang, output_type="dict", config=tesseract_config
+    )
+    words, left, top, width, height = (
+        data["text"],
+        data["left"],
+        data["top"],
+        data["width"],
+        data["height"],
+    )
+
+    # filter empty words and corresponding coordinates
+    irrelevant_indices = [idx for idx, word in enumerate(words) if not word.strip()]
+    words = [word for idx, word in enumerate(words) if idx not in irrelevant_indices]
+    left = [coord for idx, coord in enumerate(left) if idx not in irrelevant_indices]
+    top = [coord for idx, coord in enumerate(top) if idx not in irrelevant_indices]
+    width = [coord for idx, coord in enumerate(width) if idx not in irrelevant_indices]
+    height = [
+        coord for idx, coord in enumerate(height) if idx not in irrelevant_indices
+    ]
+
+    # turn coordinates into (left, top, left+width, top+height) format
+    actual_boxes = []
+    for x, y, w, h in zip(left, top, width, height):
+        actual_box = [x, y, x + w, y + h]
+        actual_boxes.append(actual_box)
+
+    image_width, image_height = image.size
+
+    # finally, normalize the bounding boxes
+    normalized_boxes = []
+    for box in actual_boxes:
+        normalized_boxes.append(normalize_box(box, image_width, image_height))
+
+    if len(words) != len(normalized_boxes):
+        raise ValueError("Not as many words as there are bounding boxes")
+
+    return words, normalized_boxes
 
 
 def main():
@@ -25,6 +82,9 @@ def main():
     wks = gc.open_by_key(os.environ["SHEET_ID"]).sheet1
     row = len(wks.col_values(1)) + 1
 
+    # store values
+    values = []
+
     # for all pdf files in this directory
     for receipt_path in os.listdir(os.getcwd()):
         if not receipt_path.endswith(".pdf"):
@@ -32,28 +92,43 @@ def main():
 
         # convert receipt pdf to jpg image
         receipt_image = pdf2image.convert_from_path(receipt_path)[0]
+        receipt_image = receipt_image.convert("RGB")
+
+        # perform ocr
+        words, boxes = apply_tesseract(receipt_image)
 
         # get total amount from receipt
-        pred = nlp(receipt_image, "What is the total amount?")
+        word_boxes = list(zip(words, boxes))
+        pred = nlp(receipt_image, "What is the total amount?", word_boxes=word_boxes)
         total = float(pred[0]["answer"])
 
-        # TODO get text from pytesseract instead and use that for zero-shot classification
-        # get image class
+        # get date from receipt
+        pred = nlp(receipt_image, "What is the date?", word_boxes=word_boxes)
+        date = parse(pred[0]["answer"])
+
+        # get receipt class
+        text = " ".join(words)
         classes = ["Grocery", "Restaurant"]
-        scores = classifier(receipt_image, candidate_labels=classes)
-        image_class = scores[0]["label"]
+        scores = classifier(text, classes)
+        image_class = scores["labels"][0]
 
-        # # write today's date in first column
-        today = datetime.date.today().strftime("%m/%d/%Y")
-        wks.update_cell(row, 1, today)
+        # store values
+        values.append((date, total, image_class))
 
-        # write the total amount in second column
-        wks.update_cell(row, 2, total)
+    # sort values by date
+    values = sorted(values, key=lambda x: x[0])
 
-        # write the image class in third column
-        wks.update_cell(row, 3, image_class)
+    # write values to sheet
+    for date, total, image_class in values:
+        wks.update(f"A{row}:C{row}", [[date.strftime("%-m/%d/%Y"), total, image_class]])
+        row += 1
 
-        # move to next row
+    # delete all pdf files
+    for receipt_path in os.listdir(os.getcwd()):
+        if receipt_path.endswith(".pdf"):
+            os.remove(receipt_path)
+
+    print("Done!")
 
 
 if __name__ == "__main__":
--- run.command
+++
@@ -1,3 +0,0 @@
-# run process.py when this file is double-clicked
-source .env/bin/activate
-python process.py
